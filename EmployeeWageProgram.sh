0faca2ea1d623996f54d2952af2b38788e775353
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-02-12 17:15:31 +0530

    [Shreyash] Refactor. changed welcome message

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-attendance=$$
-checkAttendance(){
-	attendance=$((RANDOM%2))
-	if((attendance==0))
-	then
-		echo "Employee is present"
-	else
-		echo "Employee is absent"
-	fi	
-}
-checkAttendance
+echo "WELCOME TO EMPLOYEE WAGE COMPUTATION "
 
