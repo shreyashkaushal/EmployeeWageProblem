bd38fec6eb7dc643294d22200718bcabd5bf53c3
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-02-12 19:16:09 +0530

    [Shreyash] Add. added switch case in use case 4

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION"
+partTimeWageCalculator(){
+	wage=20
+	partTimeHour=4
+	#echo "$(($wage*$partTimeHour))"
+	partTimeWage=$(($wage*$partTimeHour))
+	return $partTimeWage
+}
+
+dailyFullyTimeWage(){
+	wage=20
+	fullHour=8
+	#echo "$(($wage*$fullHour))"
+	fullTimeWage=$(($wage*$fullHour))
+	return $fullTimeWage
+}
+
+echo "Enter the choice to calculate wages "
+echo "1. full time  2. part time "
+while :
+do
+	read input
+	case $input in
+		1) dailyFullyTimeWage
+           ret=$?
+           echo $ret
+          ;;
+        2) partTimeWageCalculator
+			ret=$?
+			echo $ret
+          ;;
+        *)
+           echo "enter valid choice" 
+	esac
+done
 
