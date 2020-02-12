256911f1615f9e43db6316a7d4457d665be0ce27
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-02-12 13:28:08 +0530

    [Shreyash] Add. Added check attendance use case

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROBLEM"
-
+attendance=$$
+checkAttendance(){
+	attendance=$((RANDOM%2))
+	if((attendance==0))
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+	fi	
+}
+checkAttendance
