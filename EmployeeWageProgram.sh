7df04e0c3086a3f3f7c1bd0d87afe96077c97b22
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-02-12 17:18:57 +0530

    [Shreyash] Refactor. check attendance if code

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -4,9 +4,9 @@ checkAttendance(){
 	attendance=$((RANDOM%2))
 	if((attendance==0))
 	then
-		echo "Employee is present"
+		echo "Employee is present today"
 	else
-		echo "Employee is absent"
+		echo "Employee is absent today"
 	fi	
 }
 checkAttendance
