20dc8ce34a54924350b7ea7d9ebaaa14014ae94b
shreyashkaushal <shreyashkaushal10@gmail.com>
2020-02-13 09:41:58 +0530

    [Shreyash] Add. Added monthly wages calculation use case

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
-echo "WELCOME TO EMPLOYEE WAGE COMPUTATION"
+#useCase5
+partTimeWageCalculator(){
+	wage=20
+	partTimeHour=4
+	partTimeWage=$(($wage*$partTimeHour))
+	return $partTimeWage
+}
 
+dailyFullyTimeWage(){
+	wage=20
+	fullHour=8
+	fullTimeWage=$(($wage*$fullHour))
+	return $fullTimeWage
+}
+
+echo "Enter the choice to calculate the monthly wages "
+echo "1. full time  2. part time "
+while :
+do
+	read input
+	case $input in
+		1) dailyFullyTimeWage
+           ret=$?
+           echo $((ret*20))
+          ;;
+        2) partTimeWageCalculator
+			ret=$?
+			echo $((ret*20))
+          ;;
+        *)
+           echo "enter valid choice" 
+	esac
+done
